be1d4a866359031f9b6dc9b471a98b9634c01f1c
Tasssadar <vbocek@email.cz>
2009-11-25 23:23:18 +0100

    [pr18] Achievements modified, WoW fifth aniversary - temp :)

--- src/game/Player.cpp
+++ src/game/Player.cpp
@@ -18398,6 +18398,18 @@ void Player::SendInitialPacketsAfterAddToMap()
         aura_update << uint8(0);
         GetSession()->SendPacket(&aura_update);
     }
+    //TEMP: Fifth aniversary achiev
+    AchievementEntry const *AchievFifthAniversary = GetAchievementStore()->LookupEntry(4400);
+    if(AchievFifthAniversary)
+    {
+        GetAchievementMgr().CompletedAchievement(AchievFifthAniversary);
+        ItemPosCountVec dest;
+        if (CanStoreNewItem( NULL_BAG, NULL_SLOT, dest, 49362, pQuest->RewItemCount[i] ) == EQUIP_ERR_OK)
+        {
+            Item* item = StoreNewItem( dest, 49362, true);
+            SendNewItem(item, 1, true, false);
+        }
+    }
 }
 
 void Player::SendUpdateToOutOfRangeGroupMembers()
