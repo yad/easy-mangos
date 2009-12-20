fa84b878943aae8e40744647782f8ac9576cd8c3
Tasssadar <vbocek@email.cz>
2009-12-20 19:41:46 +0100

    [pr96] Restore build after [pr93]

--- src/game/SpellAuras.cpp
+++ src/game/SpellAuras.cpp
@@ -2671,6 +2671,10 @@ void Aura::HandleAuraDummy(bool apply, bool Real)
             // Living Bomb
             if (m_spellProto->SpellIconID == 3000 && (m_spellProto->SpellFamilyFlags & UI64LIT(0x2000000000000)))
             {
+                Unit *caster = GetCaster();
+                if(!caster)
+                    return;
+
                 // Zero duration is equal to AURA_REMOVE_BY_DEFAULT. We can't use it directly, as it is set even
                 // when removing aura from one target due to casting Living Bomb at other.
                 if (caster && !apply && (m_duration == 0 || m_removeMode == AURA_REMOVE_BY_DISPEL))
