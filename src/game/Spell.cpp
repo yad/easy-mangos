d56047b1ecd8e59f1591eb830087a9e9562bd7d6
Tasssadar <vbocek@email.cz>
2010-01-14 15:38:15 +0100

    [pr151] Missing break;
    Hunter/Warlock summon a hunter/summon pet first, then can't summon a mini pet.

--- src/game/Spell.cpp
+++ src/game/Spell.cpp
@@ -4891,6 +4891,7 @@ SpellCastResult Spell::CheckCast(bool strict)
                             return SPELL_FAILED_ALREADY_HAVE_CHARM;
                     }
                 }
+                break;
             }
             // Not used for summon?
             case SPELL_EFFECT_SUMMON_PHANTASM:
