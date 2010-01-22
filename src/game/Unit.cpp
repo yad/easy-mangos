5dfd54dff745d2f7019f858f7168cc6e885b6003
Tasssadar <vbocek@email.cz>
2010-01-22 23:27:18 +0100

    [pr170] Shattering throw remove immunity effect fixed

--- src/game/Unit.cpp
+++ src/game/Unit.cpp
@@ -2852,7 +2852,14 @@ SpellMissInfo Unit::SpellHitResult(Unit *pVictim, SpellEntry const *spell, bool 
         {
             //Shattering Throw
             if(spell->Id == 64382)
-                pVictim->RemoveAurasAtMechanicImmunity(MECHANIC_IMMUNE_SHIELD-1, 0);
+            {
+                // remove immunity effects
+                pVictim->RemoveAurasDueToSpell(642); // Divine Shield
+                pVictim->RemoveAurasDueToSpell(1022); // Hand of Protection rank 1
+                pVictim->RemoveAurasDueToSpell(5599); // Hand of Protection rank 2
+                pVictim->RemoveAurasDueToSpell(10278); // Hand of Protection rank 3
+                pVictim->RemoveAurasDueToSpell(45438); // Ice Block
+            }
             //Deep Freeze dmg if immune to stun
             else if(spell->Id == 44572)
                 CastSpell(pVictim, 71757, true);
