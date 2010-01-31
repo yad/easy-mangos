14758d81463ec52d873adc1a5d7bee781c48bd35
Tasssadar <vbocek@email.cz>
2010-01-31 17:06:47 +0100

    [pr193] Add some script for Dancing Rune weapon

--- src/game/Unit.cpp
+++ src/game/Unit.cpp
@@ -6871,7 +6871,16 @@ bool Unit::HandleDummyAuraProc(Unit *pVictim, uint32 damage, Aura* triggeredByAu
                     return false;
                 // TODO: wite script for this "fights on its own, doing the same attacks"
                 // NOTE: Trigger here on every attack and spell cast
-                return false;
+                Pet* runeBlade = FindGuardianWithEntry(27893);
+                if (!runeBlade)
+                   return false;
+                else
+                {
+                    // only melee based spells?
+                    if(procSpell)
+                        runeBlade->CastSpell(pVictim,procSpell,true,castItem,triggeredByAura);
+                    return true;
+                }
             }
             // Mark of Blood
             if (dummySpell->Id == 49005)
