ea7d231d90b90b820b2cd4dbac1c06e13aedcc95
yad <easy-mangos@live.fr>
2010-02-14 11:36:51 +0100

    sql query

---
+++ sql/updates/99999_03_characters_dualspe.sql
@@ -0,0 +1,2 @@
+ALTER TABLE `characters` CHANGE `speccount` `specCount` TINYINT(3) UNSIGNED NOT NULL DEFAULT 1;
+ALTER TABLE `characters` CHANGE `activeSpec` `activeSpec` TINYINT(3) UNSIGNED NOT NULL DEFAULT 0;
