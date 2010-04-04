00db588137d4610730471a54dd399d76e138bcf7
balrok <balrok@getmangos.com>
2010-04-04 23:11:39 +0200

    this branch currently doesn't compile
    i just want to show the basic idea
    i think it will be better to implement the type=29
    first
    
    this can be used for eye of the storm first

--- src/game/OutdoorPvP.h
+++ src/game/OutdoorPvP.h
@@ -131,7 +131,7 @@ class OutdoorPvPObjective
         virtual bool CanTalkTo(Player * /*plr*/, Creature * /*c*/, GossipOption & /*gso*/) { return false; }
 
         void DeleteSpawns();
-        void IsSpawned() { return m_spawned; }
+        bool IsSpawned() { return m_spawned; }
     protected:
 
         bool AddCapturePoint(uint32 entry, float x, float y, float z, float o, float rotation0, float rotation1, float rotation2, float rotation3);
