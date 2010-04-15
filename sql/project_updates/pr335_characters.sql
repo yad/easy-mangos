a660f2b5baa8fbedeae8252c275f94e25dd9b6a0
yad <easy-mangos@live.fr>
2010-04-15 20:57:32 +0200

    fix sql

---
+++ sql/project_updates/pr335_characters.sql
@@ -0,0 +1,5 @@
+CREATE TABLE `character_battleground_status` (
+  `guid` int(11) unsigned NOT NULL default '0' COMMENT 'Global Unique Identifier',
+  `daily_bg` BIGINT(20) NOT NULL default '0',
+  PRIMARY KEY  (`guid`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
