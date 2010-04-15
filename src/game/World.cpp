0da3fc77eb13364be3f2d80868068dd83172a479
Tasssadar <vbocek@email.cz>
2010-04-15 19:53:27 +0200

    [pr348] Implement world diff time
    Hello, trinity core... :)

--- src/game/World.cpp
+++ src/game/World.cpp
@@ -85,6 +85,7 @@ World::World()
     m_ShutdownTimer = 0;
     m_gameTime=time(NULL);
     m_startTime=m_gameTime;
+    world_diff_time = 0;
     m_maxActiveSessionCount = 0;
     m_maxQueuedSessionCount = 0;
     m_resultQueue = NULL;
@@ -1370,6 +1371,9 @@ void World::DetectDBCLang()
 /// Update the World !
 void World::Update(uint32 diff)
 {
+    //World diff time, showed in .s info, for lag detect...
+    world_diff_time = diff;
+
     ///- Update the different timers
     for(int i = 0; i < WUPDATE_COUNT; ++i)
     {
