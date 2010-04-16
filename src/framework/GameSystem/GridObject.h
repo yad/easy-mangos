f65df31134b1d20123d7916d9c93f37743f143d8
SilverIce <slifeleaf@gmail.com>
2010-04-16 11:03:55 +0300

    changes in GridObject structure

--- src/framework/GameSystem/GridObject.h
+++ src/framework/GameSystem/GridObject.h
@@ -3,79 +3,79 @@
 #ifndef GRID_OBJECT_H
 #define GRID_OBJECT_H
 
-template<typename _Type > class GridReference;
+#include "GridReference.h"
 
 template<typename _GridType >
 class MANGOS_DLL_SPEC GridObjBase
 {
     template<typename _Type > friend class Type;
-    _GridType * m_grid;
+    //static _GridType * null_grid = 0;
+    _GridType ** _m_grid;
 
 protected:
-    GridObjBase( ) : m_grid(0), m_isActiveObject(false), container_type(false) {}
+    GridObjBase( ) : _m_grid(0), m_isActiveObject(false), container_type(false) {}
     bool m_isActiveObject;
     bool container_type;    // grid container type - false, world type container - true
 
 public:
-    _GridType* GetGrid() const { return m_grid; }
+    _GridType* GetGrid() const { return *_m_grid; }
     bool isActiveObject() const { return m_isActiveObject; }
-    /*void SetActive()
-    {
-        if(m_isActiveObject == true)
-            return;
-        m_isActiveObject = true;
-        if(m_grid != 0)
-            m_grid->IncrActiveCount();
-    }
-
-    void SetPassive()
-    {
-        if(m_isActiveObject == false)
-            return;
-        m_isActiveObject = false;
-        if(m_grid != 0)
-            m_grid->DecrActiveCount();
-    }*/
 
     template<typename _Type >
-    class MANGOS_DLL_SPEC Type
+    class MANGOS_DLL_SPEC Type : protected GridReference<_Type >
     {
     public:
 
-        GridReference<_Type>& GetGridRef() { return m_ref; }
-        void RemoveFromGrid() { SetGrid(NULL);}
+        GridReference<_Type>& GetGridRef() { return *this; }
+        void RemoveFromGrid()
+        {
+            if(m_base.container_type)
+            {
+                if (m_grid != 0)
+                    m_grid->RemoveWorldObject(&m_base);
+            }
+            else
+            {
+                if (m_grid != 0)
+                    m_grid->RemoveGridObject(&m_base);
+            }
+
+            m_grid = 0;
+        }
         void SetGrid(_GridType *type)
         {
             if(m_base.container_type)
             {
-                if (m_grid_link != 0)
-                    m_grid_link->RemoveWorldObject(&m_base);
-                m_grid_link = type;
-                if (m_grid_link != 0)
-                    m_grid_link->AddWorldObject(&m_base);
+                if (m_grid != 0)
+                    m_grid->RemoveWorldObject(&m_base);
+                m_grid = type;
+                if (m_grid != 0)
+                    m_grid->AddWorldObject(&m_base);
             }
             else
             {
-                if (m_grid_link != 0)
-                    m_grid_link->RemoveGridObject(&m_base);
-                m_grid_link = type;
-                if (m_grid_link != 0)
-                    m_grid_link->AddGridObject(&m_base);
+                if (m_grid != 0)
+                    m_grid->RemoveGridObject(&m_base);
+                m_grid = type;
+                if (m_grid != 0)
+                    m_grid->AddGridObject(&m_base);
             }
         }
 
     protected:
-        Type(_Type* ty) : m_base(*ty), m_grid_link(ty->m_grid) {} //_Type should inherit from GridObjBase
+        Type(_Type* ty) : m_base(*ty), m_grid(0) { ty->_m_grid = &m_grid; } //_Type should inherit from GridObjBase
 
     private:
-        GridReference<_Type> m_ref;
+        void targetObjectDestroyLink()
+        {
+            GridReference<_Type >::targetObjectDestroyLink();
+            m_grid = 0;
+        }
+
         _Type & m_base;
-        _GridType *& m_grid_link;// for fast access to m_base members or nice view at least
+        _GridType * m_grid;
     };
 };
-
-
-
 
 
 #endif
