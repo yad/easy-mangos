2d6c1d5bbbd9c585060de4ddc8726b5462c96e17
reeshack <richard.galis@gmail.com>
2010-04-16 17:28:47 +0200

    [pr352] Update talent 1454 scaling
    Signed-off-by: reeshack <richard.galis@gmail.com>

--- src/game/Level0.cpp
+++ src/game/Level0.cpp
@@ -104,7 +104,7 @@ bool ChatHandler::HandleServerInfoCommand(const char* /*args*/)
     PSendSysMessage(LANG_USING_EVENT_AI,sWorld.GetCreatureEventAIVersion());
     PSendSysMessage(LANG_CONNECTED_USERS, activeClientsNum, maxActiveClientsNum, queuedClientsNum, maxQueuedClientsNum);
     PSendSysMessage(LANG_UPTIME, str.c_str());
-    SendSysMessage("Revision [16.4.2010][pr351] - MaNGOS modified for Valhalla Server");
+    SendSysMessage("Revision [16.4.2010][pr352] - MaNGOS modified for Valhalla Server");
     SendSysMessage("GIT: http://github.com/Tasssadar/Valhalla-Project/commits");
     SendSysMessage("Changelog: http://valhalla-team.com/web/changelog.php");
     PSendSysMessage("Diff time: %u", sWorld.GetDiffTime());
