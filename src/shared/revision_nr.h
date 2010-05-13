f45ffead1d0f2e2a175630fcf0bd9792963fcf77
Tasssadar <vbocek@email.cz>
2010-05-13 20:25:49 +0200

    [pr410] Use relaxed Anti-cheat (thx nickless, Insider42, FH3095 and whomsoever developed this patch)
    * http://getmangos.com/community/showthread.php?3625-patch-Relaxed-Anti-cheat

--- src/shared/revision_nr.h
+++ src/shared/revision_nr.h
@@ -1,6 +1,6 @@
 #ifndef __REVISION_NR_H__
 #define __REVISION_NR_H__
  #define REVISION_NR "9857"
- #define REVISION_VP "pr409"
+ #define REVISION_VP "pr410"
  #define REVISION_VP_DATE "13.5.2010"
 #endif // __REVISION_NR_H__
