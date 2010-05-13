1037e7317b79f82c9132d115b245308aae31887a
Tasssadar <vbocek@email.cz>
2010-05-13 22:48:52 +0200

    [pr413] Copy Z axis check from Random movement generator to Confused movement generator

--- src/shared/revision_nr.h
+++ src/shared/revision_nr.h
@@ -1,6 +1,6 @@
 #ifndef __REVISION_NR_H__
 #define __REVISION_NR_H__
  #define REVISION_NR "9890"
- #define REVISION_VP "pr412"
+ #define REVISION_VP "pr413"
  #define REVISION_VP_DATE "13.5.2010"
 #endif // __REVISION_NR_H__
