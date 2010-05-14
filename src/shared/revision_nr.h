57d200ac44750b9ceec337611fef1133609a09a3
KillerFrca <killerfrca@himym.cz>
2010-05-14 02:12:16 +0200

    [pr414] Fix mount-type affecting spell 71342
    Signed-off-by: KillerFrca <killerfrca@himym.cz>

--- src/shared/revision_nr.h
+++ src/shared/revision_nr.h
@@ -1,6 +1,6 @@
 #ifndef __REVISION_NR_H__
 #define __REVISION_NR_H__
  #define REVISION_NR "9890"
- #define REVISION_VP "pr413"
- #define REVISION_VP_DATE "13.5.2010"
+ #define REVISION_VP "pr414"
+ #define REVISION_VP_DATE "14.5.2010"
 #endif // __REVISION_NR_H__
