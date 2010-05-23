dadcdac975365e27fa5742e9b1e28699017c5448
yad <easy-mangos@live.fr>
2010-05-23 13:38:00 +0200

    sql typo

--- sql/project_updates/pr212_mangos.sql
+++ sql/project_updates/pr212_mangos.sql
@@ -8,10 +8,9 @@ values('43','2010-01-03 06:00:00','2020-01-03 09:00:00','5184000','1440','0','Da
 DELETE FROM creature_questrelation WHERE quest IN (11335, 11336, 11337, 11338, 11339, 11340, 11341, 11342, 13407, 13476, 14164, 13405, 13478, 14163, 11336, 11340);
 UPDATE quest_template SET ExclusiveGroup = 11335, RewHonorAddition  = 20, RewArenaPoints = 25 WHERE entry IN (11335, 11336, 11337, 11338, 13405, 14163);
 UPDATE quest_template SET ExclusiveGroup = 11339, RewHonorAddition  = 20, RewArenaPoints = 25 WHERE entry IN (11339, 11340, 11341, 11342, 13407, 14164);
-INSERT INTO game_event_creature_quest VALUES
-(15351, 11335, 41),
-(15351, 11337, 42),
-(15351, 11338, 43),
-(15350, 11339, 41),
-(15350, 11341, 42),
-(15350, 11342, 43);
+INSERT IGNORE INTO game_event_creature_quest VALUES (15351, 11335, 41);
+INSERT IGNORE INTO game_event_creature_quest VALUES (15351, 11337, 42);
+INSERT IGNORE INTO game_event_creature_quest VALUES (15351, 11338, 43);
+INSERT IGNORE INTO game_event_creature_quest VALUES (15350, 11339, 41);
+INSERT IGNORE INTO game_event_creature_quest VALUES (15350, 11341, 42);
+INSERT IGNORE INTO game_event_creature_quest VALUES (15350, 11342, 43);
