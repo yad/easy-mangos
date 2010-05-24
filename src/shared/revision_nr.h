a70eee72edd34e597b622adf82414f892a26be0f
Tasssadar <vbocek@email.cz>
2010-05-24 19:09:14 +0200

    [pr439] Gift of the naaru and Renewed hope should not stack

--- src/shared/revision_nr.h
+++ src/shared/revision_nr.h
@@ -1,6 +1,6 @@
 #ifndef __REVISION_NR_H__
 #define __REVISION_NR_H__
  #define REVISION_NR "9970"
- #define REVISION_VP "pr438"
+ #define REVISION_VP "pr439"
  #define REVISION_VP_DATE "24.5.2010"
 #endif // __REVISION_NR_H__
