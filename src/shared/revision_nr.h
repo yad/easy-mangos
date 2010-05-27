d92cd26be3077549e328d980226f9b5c14d49cfd
reeshack <richard.galis@gmail.com>
2010-05-27 23:06:36 +0200

    [pr445] Attempt to fix 18461 (and consequently 1856)
    Signed-off-by: reeshack <richard.galis@gmail.com>

--- src/shared/revision_nr.h
+++ src/shared/revision_nr.h
@@ -1,6 +1,6 @@
 #ifndef __REVISION_NR_H__
 #define __REVISION_NR_H__
  #define REVISION_NR "9970"
- #define REVISION_VP "pr444"
- #define REVISION_VP_DATE "26.5.2010"
+ #define REVISION_VP "pr445"
+ #define REVISION_VP_DATE "27.5.2010"
 #endif // __REVISION_NR_H__
