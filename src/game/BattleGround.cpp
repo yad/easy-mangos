d0059867162c6ef8825e440e62035264f915d298
KillerFrca <killerfrca@himym.cz>
2010-05-31 11:31:20 +0200

    [pr459] Implement reduce healing in battlegrounds and arenas, and fix resilience according to 3.3 patch (thx MrLama)
    Signed-off-by: KillerFrca <killerfrca@himym.cz>

--- src/game/BattleGround.cpp
+++ src/game/BattleGround.cpp
@@ -1113,6 +1113,9 @@ void BattleGround::RemovePlayerAtLeave(uint64 guid, bool Transport, bool SendPac
             plr->ClearAfkReports();
 
             if(!team) team = plr->GetTeam();
+            
+            // remove arena/battleground specific auras
+            plr->RemoveAurasDueToSpell(SPELL_AURA_PVP_HEALING); 
 
             // if arena, remove the specific arena auras
             if (isArena())
@@ -1308,6 +1311,9 @@ void BattleGround::AddPlayer(Player *plr)
     WorldPacket data;
     sBattleGroundMgr.BuildPlayerJoinedBattleGroundPacket(&data, plr);
     SendPacketToTeam(team, &data, plr, false);
+
+    // add arena/battleground specific auras
+    plr->CastSpell(plr, SPELL_AURA_PVP_HEALING,true); 
 
     // add arena specific auras
     if (isArena())
