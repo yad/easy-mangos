c27c29ee8cef932d713558c295365546af1e9b74
faramir118 <faramir118@gmail.com>
2010-05-30 20:22:03 -0500

    move inline methods to header

--- contrib/mmap/src/MapBuilder.h
+++ contrib/mmap/src/MapBuilder.h
@@ -108,6 +108,30 @@ namespace MMAP
             bool shouldSkipMap(uint32 mapID);
             bool isTransportMap(uint32 mapID);
 
+            inline unsigned int nextPow2(unsigned int v)
+            {
+                v--;
+                v |= v >> 1;
+                v |= v >> 2;
+                v |= v >> 4;
+                v |= v >> 8;
+                v |= v >> 16;
+                v++;
+                return v;
+            }
+
+            inline unsigned int ilog2(unsigned int v)
+            {
+                unsigned int r;
+                unsigned int shift;
+                r = (v > 0xffff) << 4; v >>= r;
+                shift = (v > 0xff) << 3; v >>= shift; r |= shift;
+                shift = (v > 0xf) << 2; v >>= shift; r |= shift;
+                shift = (v > 0x3) << 1; v >>= shift; r |= shift;
+                r |= (v >> 1);
+                return r;
+            }
+
             IVMapManager* m_vmapManager;
             TileBuilder* m_tileBuilder;
 
