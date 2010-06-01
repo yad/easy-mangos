cd9df17875d41e4785de9d3abe07285ed75ddb35
KillerFrca <killerfrca@himym.cz>
2010-06-02 00:13:17 +0200

    [pr463] Personal rating should keep up with team rating
    Signed-off-by: KillerFrca <killerfrca@himym.cz>

--- src/shared/revision_nr.h
+++ src/shared/revision_nr.h
@@ -1,6 +1,6 @@
 #ifndef __REVISION_NR_H__
 #define __REVISION_NR_H__
  #define REVISION_NR "10011"
- #define REVISION_VP "pr462"
+ #define REVISION_VP "pr463"
  #define REVISION_VP_DATE "31.5.2010"
 #endif // __REVISION_NR_H__
