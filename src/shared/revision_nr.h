60c6fa59b30c43eabd46fd0e45b68757bdf0387e
Tasssadar <vbocek@email.cz>
2010-06-06 23:24:45 +0200

    [pr476] Really stop moving on fear/confused remove

--- src/shared/revision_nr.h
+++ src/shared/revision_nr.h
@@ -1,6 +1,6 @@
 #ifndef __REVISION_NR_H__
 #define __REVISION_NR_H__
  #define REVISION_NR "10028"
- #define REVISION_VP "pr475"
+ #define REVISION_VP "pr476"
  #define REVISION_VP_DATE "6.6.2010"
 #endif // __REVISION_NR_H__
