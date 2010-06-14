17abb9f9da2c5801b411ba3a55c16983e360fb72
KillerFrca <killerfrca@himym.cz>
2010-06-14 11:16:20 +0200

    [pr495] Pet's deathstate must be saved to DB
    Signed-off-by: KillerFrca <killerfrca@himym.cz>

--- src/shared/revision_nr.h
+++ src/shared/revision_nr.h
@@ -1,6 +1,6 @@
 #ifndef __REVISION_NR_H__
 #define __REVISION_NR_H__
  #define REVISION_NR "10049"
- #define REVISION_VP "pr494"
- #define REVISION_VP_DATE "13.6.2010"
+ #define REVISION_VP "pr495"
+ #define REVISION_VP_DATE "14.6.2010"
 #endif // __REVISION_NR_H__
