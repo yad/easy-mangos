26076ed6e93b9fd7d48bf5b6e440b0da02c93318
KillerFrca <killerfrca@himym.cz>
2010-06-19 23:29:24 +0200

    [pr517] SendInitWorldStates for Dalaran Sewers
    Signed-off-by: KillerFrca <killerfrca@himym.cz>

--- src/shared/revision_nr.h
+++ src/shared/revision_nr.h
@@ -1,6 +1,6 @@
 #ifndef __REVISION_NR_H__
 #define __REVISION_NR_H__
  #define REVISION_NR "10054"
- #define REVISION_VP "pr516"
+ #define REVISION_VP "pr517"
  #define REVISION_VP_DATE "19.6.2010"
 #endif // __REVISION_NR_H__
