31f336d6bd7819509662946e9573bcf93c31049b
Wowka321 <galaxys@inbox.ru>
2010-07-11 15:33:25 +0300

    Anti "DeathBug"

--- src/game/Player.cpp
+++ src/game/Player.cpp
@@ -1492,6 +1492,9 @@ void Player::Update( uint32 p_time )
             RegenerateAll();
     }
 
+    if (!isAlive() && !HasFlag(PLAYER_FLAGS, PLAYER_FLAGS_GHOST))
+        SetHealth(0);
+
     if (m_deathState == JUST_DIED)
     {   // Prevent death of jailed players
         if (!m_jail_isjailed) KillPlayer();
