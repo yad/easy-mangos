50f4287c37425406754ce0b5f70243da3c27cda2
Wowka321 <galaxys@inbox.ru>
2010-07-11 22:53:09 +0300

    Duel features ))

--- src/game/Player.cpp
+++ src/game/Player.cpp
@@ -7006,7 +7006,14 @@ void Player::DuelComplete(DuelCompleteType type)
     {
         GetAchievementMgr().UpdateAchievementCriteria(ACHIEVEMENT_CRITERIA_TYPE_LOSE_DUEL, 1);
         if (duel->opponent)
-            duel->opponent->GetAchievementMgr().UpdateAchievementCriteria(ACHIEVEMENT_CRITERIA_TYPE_WIN_DUEL, 1);
+            {
+                duel->opponent->GetAchievementMgr().UpdateAchievementCriteria(ACHIEVEMENT_CRITERIA_TYPE_WIN_DUEL, 1);
+                //Credit for quest Death's Challenge
+                if (getClass() == CLASS_DEATH_KNIGHT && duel->opponent->GetQuestStatus(12733) == QUEST_STATUS_INCOMPLETE)
+                    duel->opponent->CastSpell(duel->opponent, 52994, true);
+                //Emote for winner. Just fo fun =)
+                duel->opponent->HandleEmoteCommand(EMOTE_ONESHOT_CHEER);
+            }
     }
 
     //Remove Duel Flag object
