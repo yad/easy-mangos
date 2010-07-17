d19ffabcadd7f9aad4dbeac10c073891d624ae2a
yad <easy-mangos@live.fr>
2010-07-17 07:50:06 +0200

    armory sql

---
+++ sql/project_updates/pr997_characters.sql
@@ -0,0 +1,5 @@
+CREATE TABLE `armory_character_stats` (
+  `guid` int(11) NOT NULL,
+  `data` longtext NOT NULL,
+  PRIMARY KEY  (`guid`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COMMENT='World of Warcraft Armory table';
---
+++ sql/project_updates/pr999_characters.sql
@@ -0,0 +1,8 @@
+CREATE TABLE `character_feed_log` (
+  `guid` int(11) NOT NULL,
+  `type` smallint(1) NOT NULL,
+  `data` int(11) NOT NULL,
+  `date` timestamp NOT NULL default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP,
+  `counter` int(11) NOT NULL,
+  PRIMARY KEY  (`guid`,`type`,`data`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
