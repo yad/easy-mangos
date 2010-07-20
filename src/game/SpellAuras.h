213ceb009c24464ef612375f41feaa4139ec4e48
Wowka321 <galaxys@inbox.ru>
2010-07-20 22:26:50 +0300

    Aura open stable (292). By Ceris

--- src/game/SpellAuras.h
+++ src/game/SpellAuras.h
@@ -366,6 +366,7 @@ class MANGOS_DLL_SPEC Aura
         void HandleAuraMirrorImage(bool Apply, bool Real);
         void HandleAllowOnlyAbility(bool Apply, bool Real);
         void HandleAuraLinked(bool Apply, bool Real);
+        void HandleAuraOpenStable(bool apply, bool Real);
 
         virtual ~Aura();
 
