886e5e50867eb34cc268c88fd5172c5b6d9b9e3c
yad <easy-mangos@live.fr>
2010-07-31 11:49:32 +0200

    few sql renamed files

--- sql/project_updates/pr999_mangos.sql
+++ sql/project_updates/pr999_mangos.sql
@@ -1,1 +1,36 @@
-INSERT INTO `mangos_string` VALUES ('11050', 'Vous avez reçu une insigne pour avoir tué un autre joueur.', null, null, null, null, null, null, null, null);
+UPDATE gameobject SET state = 0 WHERE id IN (SELECT entry FROM gameobject_template WHERE type = 0 AND data0 = 1);
+UPDATE creature_template SET unit_flags=unit_flags&~2048 WHERE unit_flags&2048=2048;
+UPDATE creature_template SET unit_flags=unit_flags&~524288 WHERE unit_flags&524288=524288;
+UPDATE creature_template SET unit_flags=unit_flags&~67108864 WHERE unit_flags&67108864=67108864;
+UPDATE creature_template SET unit_flags=unit_flags&~8388608 WHERE unit_flags&8388608=8388608;
+UPDATE creature, creature_template SET creature.curhealth=creature_template.minhealth,creature.curmana=creature_template.minmana WHERE creature.id=creature_template.entry and creature_template.RegenHealth = '1';
+UPDATE creature_template SET dynamicflags = dynamicflags&~4;
+UPDATE creature_template SET npcflag = npcflag&~16777216; -- UNIT_NPC_FLAG_SPELLCLICK
+UPDATE gameobject_template SET flags=flags&~4 WHERE type IN (2,19,17);
+UPDATE creature SET MovementType = 0 WHERE spawndist = 0 AND MovementType=1;
+UPDATE creature SET spawndist=0 WHERE MovementType=0;
+UPDATE quest_template SET SpecialFlags=SpecialFlags|1 WHERE QuestFlags=QuestFlags|4096;
+UPDATE quest_template SET SpecialFlags=SpecialFlags|1 WHERE QuestFlags=QuestFlags|32768;
+DELETE FROM go,gt USING gameobject go LEFT JOIN gameobject_template gt ON go.id=gt.entry WHERE gt.entry IS NULL;
+DELETE FROM gi,gt USING gameobject_involvedrelation gi LEFT JOIN gameobject_template gt ON gi.id=gt.entry WHERE gt.entry IS NULL;
+DELETE FROM gqr,gt USING gameobject_questrelation gqr LEFT JOIN gameobject_template gt ON gqr.id=gt.entry WHERE gt.entry IS NULL;
+DELETE FROM ge,go USING game_event_gameobject ge LEFT JOIN gameobject go ON ge.guid=go.guid WHERE go.guid IS NULL;
+DELETE FROM gameobject_scripts WHERE id NOT IN (SELECT guid FROM gameobject);
+DELETE FROM gameobject_scripts WHERE command in (11, 12) and datalong NOT IN (SELECT guid FROM gameobject);
+DELETE FROM gameobject_battleground WHERE guid NOT IN (SELECT guid FROM gameobject);
+DELETE FROM creature_battleground WHERE guid NOT IN (SELECT guid FROM creature);
+DELETE FROM creature_addon WHERE guid NOT IN (SELECT guid FROM creature);
+UPDATE creature_addon SET moveflags=moveflags &~ 0x00000100; -- SPLINEFLAG_DONE
+UPDATE creature_addon SET moveflags=moveflags &~ 0x00000800; -- SPLINEFLAG_TRAJECTORY (can crash client)
+UPDATE creature_addon SET moveflags=moveflags &~ 0x00200000; -- SPLINEFLAG_UNKNOWN3 (can crash client)
+UPDATE creature_addon SET moveflags=moveflags &~ 0x08000000;
+UPDATE creature_template_addon SET moveflags=moveflags &~ 0x00000100;
+UPDATE creature_template_addon SET moveflags=moveflags &~ 0x00000800;
+UPDATE creature_template_addon SET moveflags=moveflags &~ 0x00200000;
+UPDATE creature_template_addon SET moveflags=moveflags &~ 0x08000000;
+DELETE FROM npc_gossip WHERE npc_guid NOT IN (SELECT guid FROM creature);
+DELETE FROM creature_movement WHERE id NOT IN (SELECT guid FROM creature);
+DELETE FROM game_event_creature WHERE guid NOT IN (SELECT guid FROM creature);
+DELETE FROM creature_questrelation WHERE id NOT IN (SELECT entry FROM creature_template);
+DELETE FROM creature_onkill_reputation WHERE creature_id NOT IN (SELECT entry FROM creature_template);
+UPDATE creature_template SET npcflag=npcflag|2 WHERE entry IN (SELECT id FROM creature_questrelation UNION SELECT id FROM creature_involvedrelation);
