485863606c3cbd52d3149353db2a7d7f96334f6a
Wowka321 <galaxys@inbox.ru>
2010-08-04 10:30:53 +0300

    Some improvements in specializacion swich

--- src/game/Player.cpp
+++ src/game/Player.cpp
@@ -22370,7 +22370,15 @@ void Player::ActivateSpec(uint8 specNum)
     if(specNum >= GetSpecsCount())
         return;
 
+    if (GetMap()->IsBattleGround() && !HasAura(44521)) // In BattleGround with no Preparation buff 	  	
+        return;
+
     UnsummonPetTemporaryIfAny();
+    UnsummonAllTotems();
+    ClearComboPointHolders();
+    ClearAllReactives();
+    RemoveAllEnchantments(TEMP_ENCHANTMENT_SLOT);
+    RemoveArenaAuras();
 
     ApplyGlyphs(false);
 
