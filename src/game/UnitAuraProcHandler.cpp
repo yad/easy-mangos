ea7caff27a2dd0487619399ee6416a3ab5d3e1b7
Wowka321 <galaxys@inbox.ru>
2010-08-20 12:26:54 +0300

    Fix spell 49203

--- src/game/UnitAuraProcHandler.cpp
+++ src/game/UnitAuraProcHandler.cpp
@@ -2633,6 +2633,12 @@ SpellAuraProcResult Unit::HandleDummyAuraProc(Unit *pVictim, uint32 damage, Aura
                 triggered_spell_id = 50526;
                 break;
             }
+            // Hungering Cold - not break from diseases
+            if (dummySpell->SpellIconID == 2797)
+            {
+                if (procSpell && procSpell->Dispel == DISPEL_DISEASE)
+                    return SPELL_AURA_PROC_FAILED;
+            }
             // Blood-Caked Blade
             if (dummySpell->SpellIconID == 138)
             {
