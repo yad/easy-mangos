4f71214694f0ed3717cd2e204d0ad4d1aa11bb04
qsa <qsa@getmangos.com>
2010-09-18 21:29:25 -0500

    Implement pathfinding in waypoint movement generator.

--- src/game/WaypointMovementGenerator.h
+++ src/game/WaypointMovementGenerator.h
@@ -30,6 +30,7 @@
 #include "WaypointManager.h"
 #include "Path.h"
 #include "Traveller.h"
+#include "PathFinder.h"
 
 #include "Player.h"
 
@@ -97,6 +98,8 @@ public PathMovementBase<Creature, WaypointPath const*>
         bool GetDestination(float& x, float& y, float& z) const { return PathMovementBase<Creature, WaypointPath const*>::GetDestination(x,y,z); }
 
         bool GetResetPosition(Creature&, float& x, float& y, float& z);
+
+        void MoveToNextNode(CreatureTraveller &traveller);
 
     private:
         TimeTrackerSmall i_nextMoveTime;
